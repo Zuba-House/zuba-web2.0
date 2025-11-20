71c9ee264e059dcbd33ba6966d68d26b7d5b1d9a
Olivier Niyonshima <niyo0030@algonquinlive.com>
2025-11-20 12:00:05 -0500

    filtering exceed error fixed

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -191,7 +191,7 @@ export const Sidebar = (props) => {
 
   return (
     <aside className="sidebar py-3  lg:py-5 static lg:sticky top-[130px] z-[50] pr-0 lg:pr-5">
-      <div className=" max-h-[60vh]  lg:overflow-visible overflow-auto  w-full">
+      <div className="max-h-[calc(100vh-180px)] overflow-y-auto overflow-x-hidden w-full">
         <div className="box">
           <h3 className="w-full mb-3 text-[16px] font-[600] flex items-center pr-5">
             Shop by Category
