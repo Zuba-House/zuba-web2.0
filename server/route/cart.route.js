3b13d3b5ffb57264cdf19ff4467af77898e35729
OlivierNDev <it.deboss019@gmail.com>
2025-12-10 18:45:36 -0500

    Fix cart route - add semicolons and test endpoint to verify route is accessible

--- server/route/cart.route.js
+++ server/route/cart.route.js
@@ -4,11 +4,22 @@ import auth, { optionalAuth } from "../middlewares/auth.js";
 
 const cartRouter = Router();
 
+// Test route to verify cart router is working
+cartRouter.get('/test', (req, res) => {
+    return res.json({ 
+        error: false, 
+        success: true, 
+        message: 'Cart router is working',
+        timestamp: new Date().toISOString()
+    });
+});
+
 // Allow guests to add to cart (will need session/cookie-based cart for guests)
 // For now, we'll allow it but guests will need to login at checkout
-cartRouter.post('/add', optionalAuth, addToCartItemController)
-cartRouter.get("/get", optionalAuth, getCartItemController)
-cartRouter.put('/update-qty', optionalAuth, updateCartItemQtyController)
-cartRouter.delete('/delete-cart-item/:id', optionalAuth, deleteCartItemQtyController)
-cartRouter.delete('/emptyCart/:id', optionalAuth, emptyCartController)
-export default cartRouter
+cartRouter.post('/add', optionalAuth, addToCartItemController);
+cartRouter.get("/get", optionalAuth, getCartItemController);
+cartRouter.put('/update-qty', optionalAuth, updateCartItemQtyController);
+cartRouter.delete('/delete-cart-item/:id', optionalAuth, deleteCartItemQtyController);
+cartRouter.delete('/emptyCart/:id', optionalAuth, emptyCartController);
+
+export default cartRouter;
